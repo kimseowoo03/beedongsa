241791f6b2aa1792a22c940d72e52cd3382fad81
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-01 12:49:38 +0900

    Feat: User type 정의

--- types/user.ts
+++ types/user.ts
@@ -1,1 +1,25 @@
 export type UserType = "client" | "educator";
+
+interface User {
+  type?: UserType;
+  name: string;
+  email: string;
+  phoneNumber: number;
+  password: string;
+  eventEnabled: boolean;
+}
+
+export interface EducatorUser extends User {
+  type: "educator";
+  educatorType: Array<"lecture" | "consulting">;
+  lectureTopic: Array<string>;
+  experience: number;
+  phoneNumber: number;
+}
+
+export interface ClientUser extends User {
+  type: "client";
+  managerName: string;
+  managerPhoneNumber: string;
+  managerEmail: string;
+}
