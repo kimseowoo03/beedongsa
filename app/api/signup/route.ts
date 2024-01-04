ade4de8147e446764f76ef396f33927ddcbd7ade
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-05 00:11:48 +0900

    Feat: Firestore 연동 테스트

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -6,8 +6,9 @@ export async function POST(request: Request) {
   try {
     //회원가입
     const { email, password } = data;
+
     const firebaseSignupRes = await fetch(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=APIKEY넣기`,
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBkZrKQK9k1rHIEPjzs9Cd59o-pnWtt6dY`,
       {
         method: "POST",
         headers: {
@@ -20,7 +21,29 @@ export async function POST(request: Request) {
         }),
       }
     );
-    console.log(firebaseSignupRes, "<<<firebaseSignupRes");
-    //firestore에 나머지 정보 저장
+    const firebaseAuthData = await firebaseSignupRes.json();
+    console.log(firebaseAuthData, "firebaseAuthData");
+    const idToken = firebaseAuthData.idToken;
+
+    const firestoreData = {
+      fields: {
+        fieldName1: { stringValue: "some tesdwqwxt" },
+      },
+    };
+
+    console.log(idToken, "토큰 챙김");
+    const firestoreRes = await fetch(
+      `https://firestore.googleapis.com/v1/projects/beedongsa/databases/(default)/documents/users/${email}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + idToken,
+        },
+        body: JSON.stringify(firestoreData),
+      }
+    );
+    const res = await firestoreRes.json();
+    console.log(res, "firestoreRes");
   } catch (error) {}
 }
