79a1bc11708c56041030cedc155284844787f1aa
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-18 23:33:53 +0900

    Feat: Firebase fields 객체를 변환해주는 함수 추가

---
+++ utils/transformFirebaseDocument.ts
@@ -0,0 +1,22 @@
+/**
+ *Firebase 문서의 fields 객체를 변환하여 지정된 타입의 객체로 반환합니다.
+ *
+ * @param fields Firebase 문서에서 응답받은 fields 객체
+ * @returns T 타입으로 지정된 객체
+ */
+export const transformFirestoreDocument = <T>(fields: any) => {
+  let transformedObject = {};
+
+  Object.keys(fields).forEach((key) => {
+    const value = fields[key];
+    if (value.hasOwnProperty("stringValue")) {
+      transformedObject[key] = value.stringValue;
+    } else if (value.hasOwnProperty("booleanValue")) {
+      transformedObject[key] = value.booleanValue;
+    } else if (value.hasOwnProperty("integerValue")) {
+      transformedObject[key] = parseInt(value.integerValue, 10);
+    }
+  });
+
+  return transformedObject as T;
+};
