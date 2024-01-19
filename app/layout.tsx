5b1d1cf71be6b7377efd5f933c81f72b1b98a8da
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-20 00:39:41 +0900

    Refactor: 최상단 layout의 토큰 요청 로직을 Page컴포넌트에서 처리로 최적화

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,9 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
 import ReactQueryConfigs from "@/configs/ReactQueryConfigs";
-import AuthHydrateAtoms from "@/configs/AuthHydrateAtoms";
 import JotaiConfigs from "@/configs/JotaiConfigs";
 
 import Header from "@/components/Organisms/Header";
-
-import { refreshTokenFetch } from "@/services/refreshTokenFetch";
 
 const notoSansKR = Noto_Sans_KR({
   weight: ["300", "400", "500", "700"],
@@ -26,19 +23,13 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const { idToken: newIDToken } = (await refreshTokenFetch()) ?? {
-    idToken: null,
-  };
-
   return (
     <html lang="en">
       <body className={notoSansKR.className}>
         <ReactQueryConfigs>
           <JotaiConfigs>
-            <AuthHydrateAtoms newIDToken={newIDToken}>
-              <Header />
-              {children}
-            </AuthHydrateAtoms>
+            <Header />
+            {children}
           </JotaiConfigs>
         </ReactQueryConfigs>
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-"use client";
-import { useAtom } from "jotai";
-import { idTokenAtom } from "@/atoms/auth/idTokenAtom";
+import { refreshTokenFetch } from "@/services/refreshTokenFetch";
 
-export default function HomePage() {
-  const [idTosken, setIdTosken] = useAtom(idTokenAtom);
-  // const [idToken, setIdToken] = useState("");
+import HomePage from "./page.client";
 
-  console.log(idTosken, "idTosken");
-  const eventHandler = () => {
-    if (!idTosken) {
-      alert("로그인한 유저만 사용할 수 있습니다.");
-    }
+export default async function Page() {
+  const { idToken, email } = (await refreshTokenFetch()) ?? {
+    idToken: null,
   };
-  return (
-    <div>
-      <h1>메인 페이지</h1>
-      {idTosken ? <p>{idTosken}</p> : <p>로딩중</p>}
-      <button onClick={eventHandler}>인증된 유저만 눌러라</button>
-    </div>
-  );
+
+  return <HomePage />;
 }
