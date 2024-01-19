6f1d2e985a46cc2e856c774041bdd5cfb55f8668
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-19 23:51:55 +0900

    Refactor: page.tsx를 서버컴포넌트로 page.client.tsx를 클라이언트 컴포넌트로 지정

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,12 +1,51 @@
-import { ProfileClient } from "@/components/Templates/ProfileClient";
-import { ProfileEducator } from "@/components/Templates/ProfileEducator";
+import { redirect } from "next/navigation";
+import { refreshTokenFetch } from "@/services/refreshTokenFetch";
+import { transformFirestoreDocument } from "@/utils/transformFirebaseDocument";
+import type { ClientUser, EducatorUser } from "@/types/user";
+import ProfilePage from "./page.client";
 
-//TODO: 선택 유형에따라 보여주는 화면 다르게 작업
-export default function ProfilePage() {
-  return (
-    <div>
-      <ProfileEducator />
-      <ProfileClient />
-    </div>
-  );
+async function getBasicUserData({
+  idToken,
+  email,
+}: {
+  idToken: string;
+  email: string;
+}) {
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+  const documentId = encodeURIComponent(email); // URL에 사용하기 위해 인코딩
+  const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default)/documents/Users/${documentId}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    });
+
+    const userDocument = await response.json();
+
+    return userDocument;
+  } catch (error) {
+    console.error("Error fetching user document:", error);
+    return null;
+  }
+}
+
+export default async function Page() {
+  const { idToken, email } = (await refreshTokenFetch()) ?? {
+    idToken: null,
+  };
+
+  //비회원인경우 홈으로 이동
+  // if (!newIDToken) {
+  //   redirect("/");
+  // }
+
+  const { fields } = await getBasicUserData({ idToken, email });
+
+  const data = transformFirestoreDocument<EducatorUser | ClientUser>(fields);
+  console.log(data, "<<<<<<<<<<<<<<<<<fetched data");
+
+  return <ProfilePage />;
 }
