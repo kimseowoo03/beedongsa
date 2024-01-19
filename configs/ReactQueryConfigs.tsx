67c7e19004f7d994c2c684a1617624701eea4d2e
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-19 17:29:32 +0900

    Feat: react-query Query Client에서 error 공통 처리 로직 추가

--- configs/ReactQueryConfigs.tsx
+++ configs/ReactQueryConfigs.tsx
@@ -1,14 +1,30 @@
 "use client";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 
 export default function ReactQueryConfigs({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const queryClient = new QueryClient();
-
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      mutations: {
+        onError: (error) => {
+          alert(`Mutation Error 임시 모달>>${error.message}`);
+        },
+      },
+    },
+    queryCache: new QueryCache({
+      onError: (error) => {
+        alert(`Query Error 임시 모달 >> ${error.message}`);
+      },
+    }),
+  });
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
