b4a44f3c426b00397ea1cf9d67a2034393e8c0f5
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-21 01:31:13 +0900

    Feat: Profile Templates에 NoticeBar 추가

--- components/Templates/Profile/index.tsx
+++ components/Templates/Profile/index.tsx
@@ -1,3 +1,4 @@
+import NoticeBar from "@/components/Atoms/ NoticeBar";
 import { ProfileClientHeader } from "@/components/Organisms/ProfileClientHeader";
 import { ProfileEducatorHeader } from "@/components/Organisms/ProfileEducatorHeader";
 import type { ClientUser, EducatorUser } from "@/types/user";
@@ -11,6 +12,10 @@ export const Profile = ({ userData }: ProfileProps) => {
     <>
       {type === "client" && <ProfileClientHeader userData={userData} />}
       {type === "educator" && <ProfileEducatorHeader userData={userData} />}
+      <NoticeBar>
+        모든 강사님들께서 올려주시는 자격사항은 비동사 전담매니저의 확인을 거친
+        후 검증된 강사님의 프로필만 업로드됩니다.
+      </NoticeBar>
     </>
   );
 };
