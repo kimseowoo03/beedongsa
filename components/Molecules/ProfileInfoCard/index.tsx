1d74b6fbe66fad6bdd0b5cd0be2e7016623270a1
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-21 00:57:28 +0900

    Feat: ClientHeader 스타일 작업 및 데이터 적용

--- components/Molecules/ProfileInfoCard/index.tsx
+++ components/Molecules/ProfileInfoCard/index.tsx
@@ -1,29 +1,64 @@
+"use client";
+
+import styled from "@emotion/styled";
+
+const InfoCard = styled.div`
+  display: flex;
+  gap: var(--gap);
+`;
+
+const Photo = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 160px;
+  height: 160px;
+  font-size: var(--font-size-l);
+  font-weight: var(--font-weight-medium);
+  background-color: var(--font-color-1);
+  color: #fff;
+`;
+
+const Name = styled.p`
+  font-size: var(--font-size-l);
+  font-weight: var(--font-weight-medium);
+  color: var(--font-color-1);
+  margin-bottom: var(--gap);
+`;
+
+const MoreInfo = styled.p`
+  font-size: var(--font-size-xs);
+  font-weight: var(--font-weight-regular);
+  color: var(--font-color-2);
+  line-height: 2;
+`;
+
 interface ProfileBasicInfoMainProps {
   children: React.ReactNode;
 }
 const ProfileInfoCardMain = ({ children }: ProfileBasicInfoMainProps) => {
-  return <div>{children}</div>;
+  return <InfoCard>{children}</InfoCard>;
 };
 
 interface ProfilePhotoProps {
   children: React.ReactNode;
 }
 const ProfilePhoto = ({ children }: ProfilePhotoProps) => {
-  return <div>{children}</div>;
+  return <Photo>{children}</Photo>;
 };
 
 interface ProfileNameProps {
   children: React.ReactNode;
 }
 const ProfileName = ({ children }: ProfileNameProps) => {
-  return <div>{children}</div>;
+  return <Name>{children}</Name>;
 };
 
 interface ProfileMoreInfoProps {
   children: React.ReactNode;
 }
 const ProfileMoreInfo = ({ children }: ProfileMoreInfoProps) => {
-  return <p>{children}</p>;
+  return <MoreInfo>{children}</MoreInfo>;
 };
 
 export const ProfileInfoCard = Object.assign(ProfileInfoCardMain, {
--- components/Organisms/ProfileClientHeader/index.tsx
+++ components/Organisms/ProfileClientHeader/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { ProfileInfoCard } from "@/components/Molecules/ProfileInfoCard";
 import type { ClientUser } from "@/types/user";
 
@@ -5,15 +6,20 @@ interface ProfileClientHeader {
   userData: ClientUser;
 }
 export const ProfileClientHeader = ({ userData }: ProfileClientHeader) => {
+  const { name, email, managerName, managerEmail, managerPhoneNumber } =
+    userData;
+
   return (
     <ProfileInfoCard>
-      <ProfileInfoCard.Photo>사진</ProfileInfoCard.Photo>
-      <ProfileInfoCard.Name>회사이름</ProfileInfoCard.Name>
-      <ProfileInfoCard.MoreInfo>회사 이메일</ProfileInfoCard.MoreInfo>
-      <ProfileInfoCard.MoreInfo>클라이언트</ProfileInfoCard.MoreInfo>
-      <ProfileInfoCard.MoreInfo>
-        담당자명 / 담당자전화번호 / 담당자이메일
-      </ProfileInfoCard.MoreInfo>
+      <ProfileInfoCard.Photo>{name.substring(0, 4)}</ProfileInfoCard.Photo>
+      <div>
+        <ProfileInfoCard.Name>{name}</ProfileInfoCard.Name>
+        <ProfileInfoCard.MoreInfo>클라이언트</ProfileInfoCard.MoreInfo>
+        <ProfileInfoCard.MoreInfo>{email}</ProfileInfoCard.MoreInfo>
+        <ProfileInfoCard.MoreInfo>
+          {managerName} | {managerPhoneNumber} | {managerEmail}
+        </ProfileInfoCard.MoreInfo>
+      </div>
     </ProfileInfoCard>
   );
 };
