fffc3cd9f417bc6ea0a41a6d6ec216697cf705ce
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-23 00:16:21 +0900

    Feat: AnnouncementForm 컴포넌트 기초 작업 추가

---
+++ components/Organisms/AnnouncementForm/index.tsx
@@ -0,0 +1,495 @@
+"use client";
+
+import { useState } from "react";
+
+import CheckboxLabel from "@/components/Molecules/CheckboxLabel";
+import InputLabel from "@/components/Molecules/InputLabel";
+
+import styled from "@emotion/styled";
+
+import useForm from "@/hooks/useForm";
+import { SelectBox } from "@/components/Molecules/SelectBox";
+
+const CITYS = [
+  "서울특별시",
+  "인천광역시",
+  "대전광역시",
+  "광주광역시",
+  "대구광역시",
+  "울산광역시",
+  "부산광역시",
+  "경기도",
+  "강원도",
+  "충청북도",
+  "충청남도",
+  "전라북도",
+  "전라남도",
+  "경상북도",
+  "경상남도",
+  "제주특별자치도",
+];
+
+const SEOUL = [
+  "강남구",
+  "강동구",
+  "강북구",
+  "강서구",
+  "관악구",
+  "광진구",
+  "구로구",
+  "금천구",
+  "노원구",
+  "도봉구",
+  "동대문구",
+  "동작구",
+  "마포구",
+  "서대문구",
+  "서초구",
+  "성동구",
+  "성북구",
+  "송파구",
+  "양천구",
+  "영등포구",
+  "용산구",
+  "은평구",
+  "종로구",
+  "중구",
+  "중랑구",
+];
+const INCHEON = [
+  "계양구",
+  "미추홀구",
+  "남동구",
+  "동구",
+  "부평구",
+  "서구",
+  "연수구",
+  "중구",
+  "강화군",
+  "옹진군",
+];
+const DAEJEON = ["대덕구", "동구", "서구", "유성구", "중구"];
+const GWANGJU = ["광산구", "남구", "동구", "북구", "서구"];
+const DAEGU = [
+  "구/군 선택",
+  "남구",
+  "달서구",
+  "동구",
+  "북구",
+  "서구",
+  "수성구",
+  "중구",
+  "달성군",
+];
+const ULSAN = ["남구", "동구", "북구", "중구", "울주군"];
+const BUSAN = [
+  "강서구",
+  "금정구",
+  "남구",
+  "동구",
+  "동래구",
+  "부산진구",
+  "북구",
+  "사상구",
+  "사하구",
+  "서구",
+  "수영구",
+  "연제구",
+  "영도구",
+  "중구",
+  "해운대구",
+  "기장군",
+];
+const GYEONGGIDO = [
+  "고양시",
+  "과천시",
+  "광명시",
+  "광주시",
+  "구리시",
+  "군포시",
+  "김포시",
+  "남양주시",
+  "동두천시",
+  "부천시",
+  "성남시",
+  "수원시",
+  "시흥시",
+  "안산시",
+  "안성시",
+  "안양시",
+  "양주시",
+  "오산시",
+  "용인시",
+  "의왕시",
+  "의정부시",
+  "이천시",
+  "파주시",
+  "평택시",
+  "포천시",
+  "하남시",
+  "화성시",
+  "가평군",
+  "양평군",
+  "여주군",
+  "연천군",
+];
+const GANGWONDO = [
+  "강릉시",
+  "동해시",
+  "삼척시",
+  "속초시",
+  "원주시",
+  "춘천시",
+  "태백시",
+  "고성군",
+  "양구군",
+  "양양군",
+  "영월군",
+  "인제군",
+  "정선군",
+  "철원군",
+  "평창군",
+  "홍천군",
+  "화천군",
+  "횡성군",
+];
+const CHUNGCHEONGBUKDO = [
+  "제천시",
+  "청주시",
+  "충주시",
+  "괴산군",
+  "단양군",
+  "보은군",
+  "영동군",
+  "옥천군",
+  "음성군",
+  "증평군",
+  "진천군",
+  "청원군",
+];
+const Chungcheongnamdo = [
+  "계룡시",
+  "공주시",
+  "논산시",
+  "보령시",
+  "서산시",
+  "아산시",
+  "천안시",
+  "금산군",
+  "당진군",
+  "부여군",
+  "서천군",
+  "연기군",
+  "예산군",
+  "청양군",
+  "태안군",
+  "홍성군",
+];
+const JEOLLABUKDO = [
+  "군산시",
+  "김제시",
+  "남원시",
+  "익산시",
+  "전주시",
+  "정읍시",
+  "고창군",
+  "무주군",
+  "부안군",
+  "순창군",
+  "완주군",
+  "임실군",
+  "장수군",
+  "진안군",
+];
+const JEOLLANAMDO = [
+  "광양시",
+  "나주시",
+  "목포시",
+  "순천시",
+  "여수시",
+  "강진군",
+  "고흥군",
+  "곡성군",
+  "구례군",
+  "담양군",
+  "무안군",
+  "보성군",
+  "신안군",
+  "영광군",
+  "영암군",
+  "완도군",
+  "장성군",
+  "장흥군",
+  "진도군",
+  "함평군",
+  "해남군",
+  "화순군",
+];
+const GYEONGSANGBUKDO = [
+  "경산시",
+  "경주시",
+  "구미시",
+  "김천시",
+  "문경시",
+  "상주시",
+  "안동시",
+  "영주시",
+  "영천시",
+  "포항시",
+  "고령군",
+  "군위군",
+  "봉화군",
+  "성주군",
+  "영덕군",
+  "영양군",
+  "예천군",
+  "울릉군",
+  "울진군",
+  "의성군",
+  "청도군",
+  "청송군",
+  "칠곡군",
+];
+const GYEONGSANGNAMDO = [
+  "거제시",
+  "김해시",
+  "마산시",
+  "밀양시",
+  "사천시",
+  "양산시",
+  "진주시",
+  "진해시",
+  "창원시",
+  "통영시",
+  "거창군",
+  "고성군",
+  "남해군",
+  "산청군",
+  "의령군",
+  "창녕군",
+  "하동군",
+  "함안군",
+  "함양군",
+  "합천군",
+];
+const JEJUDO = ["서귀포시", "제주시"];
+
+const CategoryOptions = styled.div`
+  .label {
+    color: var(--font-color-1);
+    font-size: var(--font-size-xxs);
+    font-weight: var(--font-weight-medium);
+    margin-bottom: 8px;
+  }
+  .label::after {
+    position: relative;
+    left: 2px;
+    top: -2px;
+    content: "*";
+    color: var(--primary-color-r);
+  }
+`;
+
+const EtcInput = styled.input`
+  width: 80px;
+  box-sizing: border-box;
+  padding: 4px;
+  border: none;
+  border-bottom: 1px solid var(--gray-sub1);
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const AnnouncementForm = () => {
+  const initialValues: Announcement = {
+    registeredEmail: "",
+    title: "",
+    category: [],
+    locationOrPlace: "시/도,구/동/면",
+    schedule: [],
+    recruitmentDeadline: "",
+    target: [],
+    personnel: undefined,
+    totalCost: undefined,
+    supportIncludedInTheCost: [],
+    detail: "",
+    desiredExperience: undefined,
+    desiredQualifications: "",
+    preferredLectureOrConsultingStyle: [],
+    AdministratorApproval: true,
+  };
+
+  const { values, setValues, handleChange } = useForm({
+    initialValues,
+  });
+
+  const [etcCategory, setEtcCategory] = useState("");
+  const [subAreas, setSubAreas] = useState([]);
+
+  return (
+    <form>
+      <InputLabel
+        label="공고제목"
+        type="text"
+        name="title"
+        placeholder="공고제목을 입력해주세요."
+        value={values.title}
+        handleChange={(event) => handleChange(event)}
+      />
+      <CategoryOptions>
+        <div className="label">카테고리</div>
+        <div>
+          <CheckboxLabel
+            label="미술/공예"
+            type="checkbox"
+            name="category"
+            id="미술/공예"
+            value="미술/공예"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("미술/공예")}
+          />
+          <CheckboxLabel
+            label="체육/건강"
+            type="checkbox"
+            name="category"
+            id="체육/건강"
+            value="체육/건강"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("체육/건강")}
+          />
+          <CheckboxLabel
+            label="음악"
+            type="checkbox"
+            name="category"
+            id="음악"
+            value="음악"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("음악")}
+          />
+          <CheckboxLabel
+            label="문화 심리"
+            type="checkbox"
+            name="category"
+            id="문화 심리"
+            value="문화 심리"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("문화 심리")}
+          />
+          <CheckboxLabel
+            label="요리/베이킹"
+            type="checkbox"
+            name="category"
+            id="요리/베이킹"
+            value="요리/베이킹"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("요리/베이킹")}
+          />
+          <CheckboxLabel
+            label="실무교육/조직문화"
+            type="checkbox"
+            name="category"
+            id="실무교육/조직문화"
+            value="실무교육/조직문화"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("실무교육/조직문화")}
+          />
+          <CheckboxLabel
+            label="외국어"
+            type="checkbox"
+            name="category"
+            id="외국어"
+            value="외국어"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("외국어")}
+          />
+          <CheckboxLabel
+            label="경영/경제/마케팅"
+            type="checkbox"
+            name="category"
+            id="경영/경제/마케팅"
+            value="경영/경제/마케팅"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("경영/경제/마케팅")}
+          />
+          <CheckboxLabel
+            label="수학/과학"
+            type="checkbox"
+            name="category"
+            id="수학/과학"
+            value="수학/과학"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("수학/과학")}
+          />
+          <CheckboxLabel
+            label="컴퓨터/IT"
+            type="checkbox"
+            name="category"
+            id="컴퓨터/IT"
+            value="컴퓨터/IT"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("컴퓨터/IT")}
+          />
+          <CheckboxLabel
+            label="취업/자기개발"
+            type="checkbox"
+            name="category"
+            id="취업/자기개발"
+            value="취업/자기개발"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("취업/자기개발")}
+          />
+          <CheckboxLabel
+            label="취미/실용/스포츠"
+            type="checkbox"
+            name="category"
+            id="취미/실용/스포츠"
+            value="취미/실용/스포츠"
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes("취미/실용/스포츠")}
+          />
+          <CheckboxLabel
+            label="기타"
+            type="checkbox"
+            name="category"
+            id={etcCategory}
+            value={etcCategory}
+            handleChange={(event) => handleChange(event)}
+            checked={values.category.includes(etcCategory)}
+          >
+            <EtcInput
+              type="text"
+              placeholder="입력 후 체크"
+              name="etcCategory"
+              value={etcCategory}
+              onChange={(e) => setEtcCategory(e.target.value)}
+              disabled={values.category.includes(etcCategory)}
+            />
+          </CheckboxLabel>
+        </div>
+      </CategoryOptions>
+      <SelectBox>
+        <SelectBox.Title activeSelect={values.locationOrPlace.split(",")[0]} />
+        <SelectBox.Select>
+          {CITYS.map((city) => (
+            <SelectBox.Option
+              setValues={setValues}
+              name="locationOrPlace"
+              key={city}
+              value={city}
+            ></SelectBox.Option>
+          ))}
+        </SelectBox.Select>
+      </SelectBox>
+      {/* <SelectBox defaultValue="구/동/면"handleChange={handleChange}>
+        <SelectBox.Title />
+        <SelectBox.Select>
+          {subAreas.map((city) => (
+            <SelectBox.Option key={city} value={city}></SelectBox.Option>
+          ))}
+        </SelectBox.Select>
+      </SelectBox> */}
+    </form>
+  );
+};
