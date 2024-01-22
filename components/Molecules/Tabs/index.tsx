3dc7abbdad9a350f689ecd99499b85c819c1be01
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-22 21:14:33 +0900

    Refactor: Tabs Compound component로 리팩토링

---
+++ components/Molecules/Tabs/index.tsx
@@ -0,0 +1,85 @@
+import styled from "@emotion/styled";
+import { createContext, useContext, useState } from "react";
+
+const TabListWrap = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  gap: var(--gap);
+  padding: 10px 0;
+`;
+
+const TabTriggerWrap = styled.li<{ isCurrent: boolean }>`
+  button {
+    background-color: #fff;
+    border: none;
+    font-size: var(--font-size-xs);
+    font-weight: var(--font-weight-regular);
+    cursor: pointer;
+    opacity: ${({ isCurrent }) => (isCurrent ? 1 : 0.5)};
+  }
+`;
+
+const TabPanelWrap = styled.div``;
+
+const TabsContext = createContext({
+  activeTab: "",
+  setActiveTab: (value: string) => {},
+});
+
+interface TabsProps {
+  defaultValue: string;
+  children: React.ReactNode;
+}
+const TabsMain = ({ defaultValue, children }: TabsProps) => {
+  const [activeTab, setActiveTab] = useState(defaultValue);
+
+  return (
+    <TabsContext.Provider value={{ activeTab, setActiveTab }}>
+      {children}
+    </TabsContext.Provider>
+  );
+};
+
+interface TabListProps {
+  children: React.ReactNode;
+}
+const TabList = ({ children }: TabListProps) => {
+  return <TabListWrap>{children}</TabListWrap>;
+};
+
+interface TabTriggerProps {
+  text: string;
+  value: string;
+}
+const TabTrigger = ({ value, text }: TabTriggerProps) => {
+  const { activeTab, setActiveTab } = useContext(TabsContext);
+
+  return (
+    <TabTriggerWrap isCurrent={value === activeTab}>
+      <button type="button" onClick={() => setActiveTab(value)}>
+        {text}
+      </button>
+    </TabTriggerWrap>
+  );
+};
+
+interface TabPanelProps {
+  value: string;
+  children: React.ReactNode;
+}
+const TabPanel = ({ value, children }: TabPanelProps) => {
+  const { activeTab } = useContext(TabsContext);
+
+  if (value !== activeTab) {
+    return null;
+  }
+
+  return <TabPanelWrap>{children}</TabPanelWrap>;
+};
+
+export const Tabs = Object.assign(TabsMain, {
+  List: TabList,
+  Trigger: TabTrigger,
+  Panel: TabPanel,
+});
--- components/Templates/Profile/index.tsx
+++ components/Templates/Profile/index.tsx
@@ -1,11 +1,12 @@
 "use client";
-import { useState } from "react";
+import Link from "next/link";
 
 import { ProfileClientHeader } from "@/components/Organisms/ProfileClientHeader";
 import { ProfileEducatorHeader } from "@/components/Organisms/ProfileEducatorHeader";
 import NoticeBar from "@/components/Atoms/ NoticeBar";
-import Tab from "@/components/Molecules/Tab";
 import AnnouncementList from "@/components/Organisms/AnnouncementList";
+import Button from "@/components/Atoms/Button";
+import { Tabs } from "@/components/Molecules/Tabs";
 
 import type { ClientUser, EducatorUser } from "@/types/user";
 
@@ -14,8 +15,6 @@ interface ProfileProps {
 }
 export const Profile = ({ userData }: ProfileProps) => {
   const { type } = userData;
-  const [activeTab, setActiveTab] = useState("공고목록");
-  const tabTitles = ["공고목록", "지원관리"];
 
   return (
     <>
@@ -26,12 +25,21 @@ export const Profile = ({ userData }: ProfileProps) => {
             모든 강사님들께서 올려주시는 자격사항은 비동사 전담매니저의 확인을
             거친 후 검증된 강사님의 프로필만 업로드됩니다.
           </NoticeBar>
-          <Tab
-            tabTitles={tabTitles}
-            activeTab={activeTab}
-            setActiveTab={setActiveTab}
-          />
-          {activeTab === "공고목록" && <AnnouncementList />}
+          <Tabs defaultValue="공고목록">
+            <Tabs.List>
+              <Tabs.Trigger value="공고목록" text="공고목록" />
+              <Tabs.Trigger value="지원관리" text="지원관리" />
+            </Tabs.List>
+            <Tabs.Panel value="공고목록">
+              <div>공고목록</div>
+            </Tabs.Panel>
+            <Tabs.Panel value="지원관리">
+              <div>지원관리</div>
+            </Tabs.Panel>
+          </Tabs>
+          <Link href="/profile/client/announcement-create">
+            임시 공고 등록 버튼
+          </Link>
         </>
       )}
 
