b50bd0d7af5a122377a9e3f386047f6fac9c9a09
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-24 17:42:46 +0900

    Refactor: DateTimeInput compound component 패턴에 맞춘 로직 추가

--- components/Molecules/DateTimeInput/index.tsx
+++ components/Molecules/DateTimeInput/index.tsx
@@ -1,6 +1,33 @@
-import React, { useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-const DateTimeInput = () => {
+interface DateTimeBoxContextType {
+  date: string;
+  setDate: React.Dispatch<React.SetStateAction<string>>;
+  startTime: string;
+  setStartTime: React.Dispatch<React.SetStateAction<string>>;
+  endTime: string;
+  setEndTime: React.Dispatch<React.SetStateAction<string>>;
+  schedule: string[];
+  setSchedule: React.Dispatch<React.SetStateAction<string[]>>;
+  handleCreateDate: () => void;
+}
+
+const DateTimeBoxContext = createContext<DateTimeBoxContextType>({
+  date: "",
+  setDate: () => {},
+  startTime: "",
+  setStartTime: () => {},
+  endTime: "",
+  setEndTime: () => {},
+  schedule: [],
+  setSchedule: () => {},
+  handleCreateDate: () => {},
+});
+
+interface DateTimeBoxMainProps {
+  children: React.ReactNode;
+}
+const DateTimeBoxMain = ({ children }: DateTimeBoxMainProps) => {
   const [date, setDate] = useState("");
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
@@ -9,48 +36,89 @@ const DateTimeInput = () => {
   const handleCreateDate = () => {
     const newEntry = `${date}/${startTime}~${endTime}`;
     setSchedule([...schedule, newEntry]);
-    console.log(newEntry, "newEntry");
+    console.log(newEntry, "newEntry", schedule, "<<schedule");
   };
 
   return (
-    <div>
-      <div>
-        <label>
-          날짜:
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </label>
-        <label>
-          시작 시간:
-          <input
-            type="time"
-            value={startTime}
-            onChange={(e) => setStartTime(e.target.value)}
-          />
-        </label>
-        <label>
-          종료 시간:
-          <input
-            type="time"
-            value={endTime}
-            onChange={(e) => setEndTime(e.target.value)}
-          />
-        </label>
-        <button type="button" onClick={handleCreateDate}>
-          추가
-        </button>
-      </div>
-
-      <ul>
-        {schedule.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-    </div>
+    <DateTimeBoxContext.Provider
+      value={{
+        date,
+        setDate,
+        startTime,
+        setStartTime,
+        endTime,
+        setEndTime,
+        schedule,
+        setSchedule,
+        handleCreateDate,
+      }}
+    >
+      {children}
+    </DateTimeBoxContext.Provider>
   );
 };
 
-export default DateTimeInput;
+const DateTimeBoxDate = () => {
+  const { date, setDate } = useContext(DateTimeBoxContext);
+  return (
+    <label>
+      날짜:
+      <input
+        type="date"
+        value={date}
+        onChange={(e) => setDate(e.target.value)}
+      />
+    </label>
+  );
+};
+
+const DateTimeBoxTime = () => {
+  const { startTime, setStartTime, endTime, setEndTime } =
+    useContext(DateTimeBoxContext);
+  return (
+    <>
+      <label>
+        시작 시간:
+        <input
+          type="time"
+          value={startTime}
+          onChange={(e) => setStartTime(e.target.value)}
+        />
+      </label>
+      <label>
+        종료 시간:
+        <input
+          type="time"
+          value={endTime}
+          onChange={(e) => setEndTime(e.target.value)}
+        />
+      </label>
+    </>
+  );
+};
+
+const DateTimeBoxCreateButton = () => {
+  const { handleCreateDate } = useContext(DateTimeBoxContext);
+  return (
+    <button type="button" onClick={handleCreateDate}>
+      추가
+    </button>
+  );
+};
+
+const DateTimeBoxScheduleList = () => {
+  const { schedule } = useContext(DateTimeBoxContext);
+  return (
+    <ul>
+      {schedule.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </ul>
+  );
+};
+export const DateTimeBox = Object.assign(DateTimeBoxMain, {
+  Date: DateTimeBoxDate,
+  Time: DateTimeBoxTime,
+  CreateButton: DateTimeBoxCreateButton,
+  ScheduleList: DateTimeBoxScheduleList,
+});
