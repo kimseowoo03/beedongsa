fc6f5800f868113dfb0298c8b53823d4a5fdf6d4
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-26 12:29:03 +0900

    Feat: Profile 사용자 유형에 따라 getQueryProfileData 함수 로직 추가

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,8 +1,14 @@
-import { redirect } from "next/navigation";
-import { refreshTokenFetch } from "@/services/refreshTokenFetch";
-import { transformFirestoreDocument } from "@/utils/transformFirebaseDocument";
-import type { ClientUser, EducatorUser } from "@/types/user";
+/**components */
 import ProfilePage from "./page.client";
+
+/**utils,  services*/
+import { transformFirestoreDocument } from "@/utils/transformFirebaseDocument";
+import { transformFirestoreDocuments } from "@/utils/transformFirestoreArrayDocuments";
+import { refreshTokenFetch } from "@/services/refreshTokenFetch";
+
+/**type */
+import type { ClientUser, EducatorUser } from "@/types/user";
+import type { Announcement } from "@/types/announcement";
 
 async function getBasicUserData({
   idToken,
@@ -32,6 +38,49 @@ async function getBasicUserData({
   }
 }
 
+async function getQueryProfileData({
+  collectionId,
+  idToken,
+  email,
+}: {
+  collectionId: "Announcements" | "Lectures";
+  idToken: string;
+  email: string;
+}): Promise<[]> {
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+  const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default)/documents:runQuery`;
+  const query = {
+    structuredQuery: {
+      from: [{ collectionId }],
+      where: {
+        fieldFilter: {
+          field: { fieldPath: "registeredEmail" },
+          op: "EQUAL",
+          value: { stringValue: email },
+        },
+      },
+    },
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(query),
+    });
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    console.error("Error fetching user document:", error);
+    return null;
+  }
+}
+
 export default async function Page() {
   const { idToken, email } = (await refreshTokenFetch()) ?? {
     idToken: null,
@@ -47,7 +96,21 @@ export default async function Page() {
   const userData = transformFirestoreDocument<EducatorUser | ClientUser>(
     fields
   );
+
+  const collectionId: "Announcements" | "Lectures" =
+    userData.type === "client" ? "Announcements" : "Lectures";
+
+  const ProfileDataArray = await getQueryProfileData({
+    collectionId,
+    idToken,
+    email,
+  });
+
+  const ProfileDatas =
+    transformFirestoreDocuments<Announcement>(ProfileDataArray);
+
+  console.log(ProfileDatas, "ProfileDatas");
   console.log("-------------page.tsx-----------");
 
-  return <ProfilePage userData={userData} />;
+  return <ProfilePage userData={userData} ProfileDatas={ProfileDatas} />;
 }
