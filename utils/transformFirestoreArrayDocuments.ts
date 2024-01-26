b3d2f197ac41fac01518a46cc9046378beda6bf5
kimseowoo03 <kimseowoo03@gmail.com>
2024-01-26 12:20:38 +0900

    Feat: Firebase 응답 배열 데이터 변환해주는 transformFirestoreDocuments 커스텀 함수 추가

---
+++ utils/transformFirestoreArrayDocuments.ts
@@ -0,0 +1,24 @@
+import { transformFirestoreDocument } from "./transformFirebaseDocument";
+
+/**
+ * Firestore에서 받은 1개 이상의 문서 데이터를 변환하여 새로운 배열을 만듭니다.
+ *
+ * @param documents Firestore에서 응답받은 문서 데이터 배열
+ * @returns 각 문서에 대한 객체 배열. 각 객체는 다음을 포함합니다:
+ *          id - 문서의 고유 ID
+ *          data - 변환된 데이터 객체, T 타입
+ */
+export const transformFirestoreDocuments = <T>(
+  documents: any[]
+): { id: string; data: T }[] => {
+  return documents.map((doc) => {
+    const docData = doc.document;
+    const transformedObject = transformFirestoreDocument<T>(docData.fields);
+    const docId = docData.name.split("/").pop();
+
+    return {
+      id: docId,
+      data: transformedObject,
+    };
+  });
+};
