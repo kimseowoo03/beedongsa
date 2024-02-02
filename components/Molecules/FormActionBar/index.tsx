ba8638670ae2140131f5c785033ba28b84fc6bf4
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-02 14:32:06 +0900

    Feat: FormActionBar추가

---
+++ components/Molecules/FormActionBar/index.tsx
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+import { useRouter } from "next/navigation";
+import { HiOutlineChevronLeft } from "react-icons/hi2";
+
+const Wrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 50px;
+  background-color: #fff;
+  border-bottom: 1px solid var(--gray-sub1);
+
+  //상단 고정
+  position: sticky;
+  z-index: 10;
+  top: 0;
+`;
+
+const BackButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  > svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+export const ActionBox = styled.div`
+  display: flex;
+`;
+
+interface FormActionBarProps {
+  children: React.ReactNode;
+}
+export const FormActionBar = ({ children }: FormActionBarProps) => {
+  const router = useRouter();
+  return (
+    <Wrap>
+      <BackButton type="button" onClick={() => router.back()}>
+        <HiOutlineChevronLeft />
+      </BackButton>
+      {children}
+    </Wrap>
+  );
+};
