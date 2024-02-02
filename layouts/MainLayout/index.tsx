ac871ec58a8b587d2451afd51304972d699e019c
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-02 14:40:45 +0900

    Fix: Header 노출 url경로 수정

--- layouts/MainLayout/index.tsx
+++ layouts/MainLayout/index.tsx
@@ -7,8 +7,8 @@ export default function MainLayout({ children }) {
   const pathname = usePathname();
 
   const isActionHeader =
-    pathname.startsWith("/profile/announcement-create") ||
-    pathname.startsWith("/profile/announcement-edit") ||
+    pathname.startsWith("/profile/client/announcement-create") ||
+    pathname.startsWith("/profile/client/announcement-edit") ||
     pathname.startsWith("/profile/temporarystorage");
 
   return (
