1e19a12dff927bce7b892385bf09482d1095ea63
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-10 18:17:44 +0900

    Chore: profile type ProfileLectureDatasType 추가

--- types/profile.ts
+++ types/profile.ts
@@ -1,7 +1,14 @@
 import type { Announcement } from "./announcement";
+import type { Lecture } from "./lecture";
 
 export interface ProfileAnnouncementDatasType {
   id: string;
   data: Announcement;
+  createTime: string;
+}
+
+export interface ProfileLectureDatasType {
+  id: string;
+  data: Lecture;
   createTime: string;
 }
