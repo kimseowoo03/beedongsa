1386ee22165e3e07c621be40f850f23d205fd238
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-12 23:40:04 +0900

    Feat: profile/edit 페이지 생성

---
+++ app/profile/edit/page.tsx
@@ -0,0 +1,20 @@
+import { refreshTokenFetch } from "@/services/refreshTokenFetch";
+import AuthHydrateAtoms from "@/configs/AuthHydrateAtoms";
+import ProfileEditPage from "./page.client";
+
+export default async function Page() {
+  const { idToken, email } = (await refreshTokenFetch()) ?? {
+    idToken: null,
+  };
+
+  //비회원인경우 홈으로 이동
+  // if (!newIDToken) {
+  //   redirect("/");
+  // }
+
+  return (
+    <AuthHydrateAtoms email={email} idToken={idToken}>
+      <ProfileEditPage />
+    </AuthHydrateAtoms>
+  );
+}
