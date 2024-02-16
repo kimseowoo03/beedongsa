84cf27375e960f0d8c896053567ae4f143252732
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-16 15:37:19 +0900

    Style: Profile 레이아웃 조정

--- components/Templates/[userID]/index.tsx
+++ components/Templates/[userID]/index.tsx
@@ -5,15 +5,15 @@ import { ProfileClientHeader } from "@/components/Organisms/ProfileClientHeader"
 import { ProfileEducatorHeader } from "@/components/Organisms/ProfileEducatorHeader";
 import NoticeBar from "@/components/Atoms/NoticeBar";
 import AnnouncementList from "@/components/Organisms/AnnouncementList";
-import Button from "@/components/Atoms/Button";
 import { Tabs } from "@/components/Molecules/Tabs";
+import LectureList from "@/components/Organisms/LectureList";
+import ProfileDetailInfo from "@/components/Organisms/ProfileDetailInfo";
 
 import type { ClientUser, EducatorUser } from "@/types/user";
 import type {
   ProfileAnnouncementDatasType,
   ProfileLectureDatasType,
 } from "@/types/profile";
-import LectureList from "@/components/Organisms/LectureList";
 
 interface ProfileProps {
   userData: EducatorUser | ClientUser;
@@ -38,6 +38,9 @@ export const Profile = ({ userData, ProfileDatas }: ProfileProps) => {
               <Tabs.Trigger value="지원관리" text="지원관리" />
             </Tabs.List>
             <Tabs.Panel value="공고목록">
+              <Link href={`/${userID}/announcement-create`}>
+                임시 공고 등록 버튼
+              </Link>
               {ProfileDatas.map((ProfileData) => {
                 return (
                   <AnnouncementList
@@ -51,9 +54,6 @@ export const Profile = ({ userData, ProfileDatas }: ProfileProps) => {
               <div>지원관리</div>
             </Tabs.Panel>
           </Tabs>
-          <Link href={`/${userID}/announcement-create`}>
-            임시 공고 등록 버튼
-          </Link>
         </>
       )}
 
@@ -64,15 +64,18 @@ export const Profile = ({ userData, ProfileDatas }: ProfileProps) => {
             모든 강사님들께서 올려주시는 자격사항은 비동사 전담매니저의 확인을
             거친 후 검증된 강사님의 프로필만 업로드됩니다.
           </NoticeBar>
-          <Tabs defaultValue="강의목록">
+          <Tabs defaultValue="상세정보">
             <Tabs.List>
               <Tabs.Trigger value="상세정보" text="상세정보" />
               <Tabs.Trigger value="강의목록" text="강의목록" />
             </Tabs.List>
             <Tabs.Panel value="상세정보">
-              <div>상세정보</div>
+              <ProfileDetailInfo userData={userData} />
             </Tabs.Panel>
             <Tabs.Panel value="강의목록">
+              <Link href={`/${userID}/lecture-create`}>
+                임시 강의 등록 버튼
+              </Link>
               {ProfileDatas.map((ProfileData) => {
                 return (
                   <LectureList key={ProfileData.id} ProfileData={ProfileData} />
@@ -80,7 +83,6 @@ export const Profile = ({ userData, ProfileDatas }: ProfileProps) => {
               })}
             </Tabs.Panel>
           </Tabs>
-          <Link href={`/${userID}/lecture-create`}>임시 강의 등록 버튼</Link>
         </>
       )}
     </>
