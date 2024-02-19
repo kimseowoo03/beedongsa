2a9ce5c602d781c54d9497c1b15563cc8d184376
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-19 18:09:32 +0900

    Feat: api/setting/portfolio api로직 수정

--- app/api/setting/portfolio/route.ts
+++ app/api/setting/portfolio/route.ts
@@ -4,44 +4,76 @@ export async function POST(request: Request) {
     throw new Error("유효하지 않은 사용자입니다.");
   }
 
+  // token으로 유저의 정보를 가져와 email에서 userID를 구한다
   const formData = await request.formData();
-  const files: File[] = [];
-  formData.forEach((value, key) => {
-    if (key === "files" && value instanceof File) {
-      files.push(value);
-    }
-  });
-
-  const url = `https://firebasestorage.googleapis.com/v1beta/projects/${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}/buckets/${process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET}:addFirebase`;
 
   try {
-    const firestoreRes = await fetch(url, {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: files[0].name,
-        contentType: files[0].type,
-        file: files[0],
-      }),
-    });
-    const firestoreUserDataRes = await firestoreRes.json();
-    console.log(
-      "------------------------console.log(firestoreRes)console.log(firestoreRes)console.log(firestoreRes)console.log(firestoreRes)"
+    const userDataRes = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idToken: token,
+        }),
+      }
     );
-    console.log(firestoreUserDataRes);
 
-    if (firestoreUserDataRes.error) {
-      throw new Error(firestoreUserDataRes.error.message);
+    const userData = await userDataRes.json();
+
+    if (userData.error) {
+      throw new Error(userData.error.message);
     }
 
-    console.log(firestoreUserDataRes, "<<firestoreUserDataRes");
+    const userID = userData.users[0].email.split("@")[0];
+
+    const filePromises = Array.from(formData.entries()).map(
+      async ([key, value]) => {
+        console.log(key, value, "<<<현재 파일");
+        if (key === "files" && value instanceof File) {
+          const url = `https://firebasestorage.googleapis.com/v0/b/${
+            process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+          }/o?uploadType=media&name=${encodeURIComponent(
+            `${userID}/portfolio/${value.name}`
+          )}`;
+
+          try {
+            const firestoreRes = await fetch(url, {
+              method: "POST",
+              headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-Type": value.type,
+              },
+              body: value,
+            });
+
+            const firestoreUserDataRes = await firestoreRes.json();
+            console.log(firestoreUserDataRes, "<<<<<<<<firestoreUserDataRes");
+
+            return firestoreUserDataRes; // Returning the response for each file
+          } catch (error) {
+            console.error(`업로드 실패한 파일명: ${value.name}`, error);
+            return { error: error.message };
+          }
+        }
+      }
+    );
+
+    const responses = await Promise.all(filePromises);
+
+    const errorResponse = responses.find((res) => res && res.error);
+
+    if (errorResponse) {
+      throw new Error(errorResponse.error);
+    }
+
+    console.log(responses, "<<responses");
 
     return new Response(
       JSON.stringify({
-        data: "",
+        data: responses,
         message: "업로드 완료",
       }),
       {
