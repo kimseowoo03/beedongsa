94159fceab88d0efacee549bd0aa8ee0f2932081
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-20 16:52:37 +0900

    Feat: firebaseConfig 설정

---
+++ app/firebaseConfig.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+
+interface FirebaseConfig {
+  apiKey: string;
+  authDomain: string;
+  projectId: string;
+  storageBucket: string;
+  messagingSenderId: string;
+  appId: string;
+}
+const firebaseConfig: FirebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
+};
+
+const app = initializeApp(firebaseConfig);
+
+const storage: FirebaseStorage = getStorage(app);
+
+//storage만 SDK 사용
+export { storage };
