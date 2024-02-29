982e13a4411ca0ef5fab4c4869aac71334215161
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-29 16:55:09 +0900

    Feat: 상세페이지 임시 데이터 작업

--- app/announcements/[announcementID]/page.client.tsx
+++ app/announcements/[announcementID]/page.client.tsx
@@ -1,14 +1,57 @@
 "use client";
 
+import { AnnouncementDetail } from "@/components/Templates/Announcements/[announcementID]";
+
+import { Announcement } from "@/types/announcement";
+import { transformFirestoreDocument } from "@/utils/transformFirebaseDocument";
+import { useQuery } from "@tanstack/react-query";
+
+import type { FirebaseDocument } from "@/types/firebaseType";
+
+interface getQueryAnnouncementDataProps {
+  announcementID: string;
+}
+async function getQueryAnnouncementData({
+  announcementID,
+}: getQueryAnnouncementDataProps): Promise<FirebaseDocument> {
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+  const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default)/documents/Announcements/${announcementID}`;
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+    });
+
+    const announcementData = await response.json();
+
+    return announcementData;
+  } catch (error) {
+    console.error("Error fetching user document:", error);
+    return null;
+  }
+}
+
 interface AnnouncementDetailPageProps {
   announcementID: string;
 }
 export default function AnnouncementDetailPage({
   announcementID,
 }: AnnouncementDetailPageProps) {
+  //쿼리 공고 가져오기
+  const { data, isLoading, error } = useQuery({
+    queryKey: [`announcement_${announcementID}`],
+    queryFn: () => getQueryAnnouncementData({ announcementID }),
+    refetchOnWindowFocus: false,
+  });
+
+  const announcementData =
+    !isLoading && data && transformFirestoreDocument<Announcement>(data.fields);
+
   return (
-    <div>
-      디테일 페이지 입니다 클릭하신 공고 아이디는 {announcementID} 입니다.
-    </div>
+    <AnnouncementDetail
+      announcementID={announcementID}
+      announcementData={announcementData}
+      isLoading={isLoading}
+    />
   );
 }
---
+++ components/Templates/Announcements/[announcementID]/index.tsx
@@ -0,0 +1,41 @@
+import { useAtom } from "jotai";
+import { inquiriesModalAtom } from "@/atoms/modal";
+
+import { Announcement } from "@/types/announcement";
+import { InquiriesModal } from "@/components/Organisms/InquiriesModal";
+
+interface AnnouncementDetailProps {
+  announcementID: string;
+  announcementData: Announcement;
+  isLoading: boolean;
+}
+export const AnnouncementDetail = ({
+  announcementID,
+  announcementData,
+  isLoading,
+}: AnnouncementDetailProps) => {
+  const [isInquiriesModal, setIsInquiriesModal] = useAtom(inquiriesModalAtom);
+
+  if (isLoading) {
+    return <p>로딩중입니다</p>;
+  }
+
+  //TODO: 스켈레톤 컴포넌트로 랜더링 구성
+  return (
+    <>
+      <div>
+        {isLoading ? "데이터 로딩중..." : announcementData.title}
+        <button type="button" onClick={() => setIsInquiriesModal(true)}>
+          문의하기
+        </button>
+      </div>
+
+      <InquiriesModal
+        inquiriesUserType="questioner"
+        inquiryPostTitle={announcementData.title}
+        inquiryPostId={announcementID}
+        responderId={announcementData.registeredEmail.split("@")[0]}
+      />
+    </>
+  );
+};
