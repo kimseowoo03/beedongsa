7d546796ee0fa0b6ca701c7e8678c9ec29f10f71
kimseowoo03 <kimseowoo03@gmail.com>
2024-02-29 16:55:27 +0900

    Chore: Inquiries type 추가

---
+++ types/inquiries.ts
@@ -0,0 +1,8 @@
+export interface Inquiries {
+  questionerId: string;
+  responderId: string;
+  inquiryPostId: string;
+  inquiryPostTitle: string;
+  questionContent: string;
+  answerContent: string;
+}
