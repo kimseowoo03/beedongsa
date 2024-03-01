3cad9d090534277b0962382906278cbce7af9759
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-01 18:36:20 +0900

    Feat: Firebase Query 요청 useSomeDataQuery 커스텀 훅 추가

---
+++ hooks/useSomeDataQuery.ts
@@ -0,0 +1,69 @@
+import { useQuery } from "@tanstack/react-query";
+
+interface FetchSomeDataProps {
+  collectionId: string;
+  fieldPath: string;
+  value: string | boolean | number;
+}
+async function fetchSomeData({
+  collectionId,
+  fieldPath,
+  value,
+}: FetchSomeDataProps) {
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+  const url = `https://firestore.googleapis.com/v1beta1/projects/${projectId}/databases/(default)/documents:runQuery`;
+
+  const getValueType = (value: string | boolean | number) => {
+    if (typeof value === "string") {
+      return { stringValue: value };
+    } else if (typeof value === "boolean") {
+      return { booleanValue: value };
+    } else if (typeof value === "number") {
+      return { integerValue: value };
+    } else {
+      throw new Error("Unsupported value type");
+    }
+  };
+
+  const query = {
+    structuredQuery: {
+      from: [{ collectionId }],
+      where: {
+        fieldFilter: {
+          field: { fieldPath },
+          op: "EQUAL",
+          value: getValueType(value),
+        },
+      },
+    },
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(query),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    return await response.json();
+  } catch (error) {
+    console.error("Error fetching approved lectures:", error);
+    return null;
+  }
+}
+
+export const useSomeDataQuery = ({
+  collectionId,
+  fieldPath,
+  value,
+}: FetchSomeDataProps) => {
+  return useQuery({
+    queryKey: ["someData", fieldPath],
+    queryFn: () =>
+      fetchSomeData({
+        collectionId,
+        fieldPath,
+        value,
+      }),
+  });
+};
