c4f9d4cbd447b69094dda27d076922b10a8d4211
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-02 14:15:18 +0900

    Chore: InquiriesQuery type 추가

--- types/inquiries.ts
+++ types/inquiries.ts
@@ -1,8 +1,21 @@
+import { QueryObserverResult } from "@tanstack/react-query";
+
 export interface Inquiries {
+  dateOfInquiry: string;
   questionerId: string;
   responderId: string;
   inquiryPostId: string;
   inquiryPostTitle: string;
   questionContent: string;
   answerContent: string;
+}
+
+export interface InquiriesQuery {
+  data: {
+    createTime: string;
+    data: Inquiries[];
+    id: string;
+  }[];
+  isLoading: boolean;
+  refetch: () => Promise<QueryObserverResult<Inquiries[], Error>>;
 }
