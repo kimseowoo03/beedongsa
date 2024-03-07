149d22d4a76c16e88ffa9d4f6f9f7e11d0e2042b
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-07 13:53:49 +0900

    Chore: Apply type 정의

---
+++ types/apply.ts
@@ -0,0 +1,16 @@
+export interface Apply {
+  sentStatus: string;
+  responseStatus: string;
+  matchConfirmationStatus: string;
+  educatorID: string;
+  educatorName: string;
+  lectureID: string;
+  attachedFileName: string;
+  managerName: string;
+  managerPhoneNumber: number;
+  managerEmail: string;
+  clientID: string;
+  announcementID: string;
+  announcementTitle: string;
+  announcementSchedule: Array<string>;
+}
