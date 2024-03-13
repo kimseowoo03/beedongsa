51d287546104ea1b522894ba5d747c5010d8858d
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-13 12:17:27 +0900

    Chore: userAtom type 설정

--- atoms/auth/index.ts
+++ atoms/auth/index.ts
@@ -1,8 +1,17 @@
+import { UserType } from "@/types/user";
 import { atom } from "jotai";
 
-export const userAtom = atom({
+interface UserAtom {
+  idToken: string | null;
+  email: string | null;
+  userID: string | null;
+  name: string | null;
+  type: UserType | null;
+}
+export const userAtom = atom<UserAtom>({
   idToken: null,
   email: null,
   userID: null,
   name: null,
+  type: null,
 });
