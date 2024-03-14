eb4aa43c96fdaa3f6d4c891e207e9b3552ba0f9e
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-14 18:50:55 +0900

    Feat: 최상단 Page에서 user Type 추가

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import { refreshTokenFetch } from "@/services/refreshTokenFetch";
 import HomePage from "./page.client";
 import AuthHydrateAtoms from "@/configs/AuthHydrateAtoms";
 
+import { getBasicUserData } from "@/utils/userData";
+import { transformFirestoreDocument } from "@/utils/transformFirebaseDocument";
+import type { ClientUser, EducatorUser } from "@/types/user";
+
 export default async function Page() {
   const { idToken, email, name } = (await refreshTokenFetch()) ?? {
     idToken: null,
@@ -10,8 +14,15 @@ export default async function Page() {
     name: null,
   };
 
+  const userID = email.split("@")[0];
+  const { fields } = await getBasicUserData({ idToken, userID });
+
+  const { type } = transformFirestoreDocument<EducatorUser | ClientUser>(
+    fields
+  );
+
   return (
-    <AuthHydrateAtoms email={email} idToken={idToken} name={name}>
+    <AuthHydrateAtoms email={email} idToken={idToken} name={name} type={type}>
       <HomePage />
     </AuthHydrateAtoms>
   );
