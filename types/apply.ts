fc36c9cc2989ad3d63e72c7f4545462af4acc116
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-14 19:05:46 +0900

    Chore: Apply type attachedFileName type 수정, applyType 추가

--- types/apply.ts
+++ types/apply.ts
@@ -2,12 +2,13 @@ export interface Apply {
   sentStatus: boolean;
   responseStatus: boolean;
   matchConfirmationStatus: boolean;
+  applyType: "registeredProfile" | "attachments";
   educatorID: string;
   educatorName: string;
   educatorPhoneNumber: string;
   educatorEmail: string;
   lectureID: string;
-  attachedFileName: string;
+  attachedFileName: Array<string>;
   managerName: string;
   managerPhoneNumber: number;
   managerEmail: string;
