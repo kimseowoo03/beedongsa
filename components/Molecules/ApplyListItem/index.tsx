d16cad5f637698f8bf8798d519b08a43d508a507
kimseowoo03 <kimseowoo03@gmail.com>
2024-03-27 18:34:44 +0900

    Feat: 지원 상세 리스트 컴포넌트 로직 추가

--- components/Molecules/ApplyListItem/index.tsx
+++ components/Molecules/ApplyListItem/index.tsx
@@ -1,4 +1,8 @@
+import { applyStatusAtom, applyStatusModalAtom } from "@/atoms/apply";
 import { Apply } from "@/types/apply";
+import { useSetAtom } from "jotai";
+import Link from "next/link";
+import { useState } from "react";
 
 interface ApplyListItemProps extends Apply {
   id: string;
@@ -14,6 +18,7 @@ export const ApplyListItem = ({
   educatorPhoneNumber,
   educatorEmail,
   lectureID,
+  lectureTitle,
   attachedFileName,
   managerName,
   managerPhoneNumber,
@@ -26,5 +31,82 @@ export const ApplyListItem = ({
   recruitmentDeadline,
   dateOfInquiry,
 }: ApplyListItemProps) => {
-  return <li>{announcementTitle}</li>;
+  const setApplyStatusModal = useSetAtom(applyStatusModalAtom);
+  const setApplyStatus = useSetAtom(applyStatusAtom);
+
+  const handleCheckApplyStatus = () => {
+    console.log("클릭");
+    setApplyStatus({
+      sentStatus,
+      responseStatus,
+      matchConfirmationStatus,
+    });
+    setApplyStatusModal(() => true);
+  };
+
+  return (
+    <li>
+      <p>{clientName}</p>
+      <div>
+        <p>
+          <Link href={`/announcements/${announcementID}`}>
+            {announcementTitle}
+          </Link>
+        </p>
+        <button type="button" onClick={handleCheckApplyStatus}>
+          지원 현황 확인
+        </button>
+      </div>
+      <div>
+        <span>{dateOfInquiry}</span>
+
+        <ApplyTypeDetail
+          applyType={applyType}
+          lectureID={lectureID}
+          lectureTitle={lectureTitle}
+          attachedFileName={attachedFileName}
+        />
+      </div>
+    </li>
+  );
+};
+const ApplyTypeDetail = ({
+  applyType,
+  lectureID,
+  lectureTitle,
+  attachedFileName,
+}) => {
+  const [isApplyTypeDetails, setIsApplyTypeDetails] = useState(false);
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={() => setIsApplyTypeDetails((prev) => !prev)}
+      >
+        {applyType === "registeredProfile" ? "프로필 지원" : "파일 지원"}
+      </button>
+
+      {isApplyTypeDetails && (
+        <ul>
+          {applyType === "registeredProfile" ? (
+            <li>
+              <span>프로필</span>
+              <Link href={`/lectures/${lectureID}`}>{lectureTitle}</Link>
+            </li>
+          ) : (
+            attachedFileName.map((fileName) => {
+              const [name, size] = fileName.split("/");
+              return (
+                <li key={name}>
+                  <span>첨부파일</span>
+                  <span>{name}</span>
+                </li>
+              );
+            })
+          )}
+        </ul>
+      )}
+    </>
+  );
 };
