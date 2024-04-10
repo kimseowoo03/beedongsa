971f6e216dc49ab260fd8776ec4a437021edc664
kimseowoo03 <kimseowoo03@gmail.com>
2024-04-10 14:03:22 +0900

    Fix: 지원내역 데이터 없을 때 처리

--- components/Organisms/ApplyList/index.tsx
+++ components/Organisms/ApplyList/index.tsx
@@ -4,6 +4,9 @@ import { useApplyQuery } from "@/hooks/[userID]/useApplyQuery";
 export const ApplyList = () => {
   const { data, isLoading } = useApplyQuery();
 
+  if (!data) {
+    return <p>지원내역이 없습니다.</p>;
+  }
   return (
     <ul>
       {data.map(({ data, id }) => {
