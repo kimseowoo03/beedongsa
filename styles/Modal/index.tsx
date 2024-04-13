254258a17ea00ef0d35968975bb3823e5d478fd1
kimseowoo03 <kimseowoo03@gmail.com>
2024-04-13 23:05:42 +0900

    Style: Modal CloseButton style 추가, Modal 640px부터는 최대 너비 높이로 채우기

--- styles/Modal/index.tsx
+++ styles/Modal/index.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import { HiOutlineXMark } from "react-icons/hi2";
 
 export const BackgroundModal = styled.div`
   overflow: auto;
@@ -16,13 +17,19 @@ export const ModalWrap = styled.div`
   top: 50%;
   left: 50%;
   z-index: 110;
-  padding: 40px;
+  padding: 30px 40px;
   border-radius: 16px;
   box-sizing: border-box;
   letter-spacing: -1px;
   background-color: var(--white);
   box-shadow: 0 0 5px 0 rgba(0, 0, 0, 0.15);
   transform: translateY(-50%) translateX(-50%) translateZ(0);
+
+  @media (max-width: 640px) {
+    width: 100%;
+    height: 100%;
+    border-radius: 0px;
+  }
 `;
 
 export const Buttons = styled.div`
@@ -33,4 +40,16 @@ export const Buttons = styled.div`
   width: calc(100% - 80px);
   left: 50%;
   transform: translateX(-50%);
+`;
+
+export const CloseButton = styled(HiOutlineXMark)`
+  position: absolute;
+  top: 30;
+  right: 40px;
+  width: 24px;
+  height: 24px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
